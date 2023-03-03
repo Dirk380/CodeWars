7f47dbcb4da1b0118ef9df0fdefbe9413216f690
Dirk380 <dirkvmunster@gmail.com>
2023-03-03 09:52:56 +0100

    EvenOrOdd

--- demo/src/main/java/com/example/EvenOrOdd.java
+++ demo/src/main/java/com/example/EvenOrOdd.java
@@ -1,17 +1,12 @@
 package com.example;
 
-public class Even_or_odd {
-
-    public static void even_or_odd(int number){
-        if (number % 2 == 0){
-            System.out.println("this number is even");
-        } else {
-            System.out.println("this number is odd");
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if(number % 2 == 0){
+            return "Even";
+        }else {
+            return "Odd";
         }
 
     }
-    public static void main(String[] args) {
-    even_or_odd(12);
-
-        }
-    }
+}
--- demo/src/main/java/com/example/Troll.java
+++ demo/src/main/java/com/example/Troll.java
@@ -1,2 +1,30 @@
-package com.example;public class Troll {
+package com.example;
+
+public class Troll {
+
+    public static void disemvowel(String str) {
+
+        //This is setting up something to add to and return.
+        String output = "";
+
+        //This iterates (steps through) the string.
+        for(int i = 0; i < str.length(); i++) {
+
+            //! means not in Java, so it checks if our current letter is not a vowel, or is a consonant.
+            if(str.charAt(i) != 'a' && str.charAt(i) != 'b') {
+
+                //Here, we add the consonant to the string we will output.
+                output = output + str.charAt(i);
+            }
+        }
+
+        //We give the output back after running through the program (The original sentence, just without vowels)
+        System.out.println(output);
+
+
+    }
+
+    public static void main(String[] args) {
+        disemvowel("AAAAbbb");
+    }
 }

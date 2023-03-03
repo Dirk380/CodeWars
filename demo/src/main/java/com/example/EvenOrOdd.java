d4c9c3c716519eacb1a58b507d9158b2feed86fb
Dirk380 <dirkvmunster@gmail.com>
2023-03-03 08:54:45 +0100

    Initial commit

---
+++ demo/src/main/java/com/example/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class Even_or_odd {
+
+    public static void even_or_odd(int number){
+        if (number % 2 == 0){
+            System.out.println("this number is even");
+        } else {
+            System.out.println("this number is odd");
+        }
+
+    }
+    public static void main(String[] args) {
+    even_or_odd(12);
+
+        }
+    }
